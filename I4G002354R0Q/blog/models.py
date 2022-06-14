9b3b9687c7f874e8fd2ad3c086aaea50490fa262
Ajenipa <jamiuajenipa@gmail.com>
2022-06-14 11:09:12 +0100

    first commit

---
+++ I4G002354R0Q/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200),
+    Text = models.TextField(max_length=10000),
+    Author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+    
